9be01f4ce345b3946a352e63da4aa4b96ee2512e
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2025-03-07 15:29:34 +0000

    adicao do cloud.js, adicao do script.js, e adicao do server.js(para teste no local)

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+// *** Inicialização do Parse ***
+Parse.serverURL = "https://parseapi.back4app.com/";
+Parse.initialize(
+  "GAcpwTn8vWR3IH6vopjoWxeMi9r0HNQpujteULei",
+  "eohDaTIobNJcVdrnRoJHmU9Sgm1i8TtsJ7HdWg1O"
+);

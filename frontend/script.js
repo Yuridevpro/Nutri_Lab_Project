30a6c988b96364c4de77b117854a94295a184048
Yuri da Silva Ferreira <142122885+Yuridevpro@users.noreply.github.com>
2025-03-08 16:47:37 +0000

    adicao da logica de refeicao no script.js

--- frontend/script.js
+++ frontend/script.js
@@ -4,3 +4,34 @@ Parse.initialize(
   "GAcpwTn8vWR3IH6vopjoWxeMi9r0HNQpujteULei",
   "eohDaTIobNJcVdrnRoJHmU9Sgm1i8TtsJ7HdWg1O"
 );
+
+// Yuri
+// *** Funções para Refeicao ***
+
+// Função para criar refeição
+async function criarRefeicao(
+  pacienteId,
+  titulo,
+  horario,
+  carboidratos,
+  proteinas,
+  gorduras
+) {
+
+  try {
+    const result = await Parse.Cloud.run("criarRefeicao", {
+      pacienteId: pacienteId,
+      titulo: titulo,
+      horario: horario,
+      carboidratos: carboidratos,
+      proteinas: proteinas,
+      gorduras: gorduras,
+    });
+    return result;
+  } catch (error) {
+    console.error("Erro ao criar refeição:", error);
+    alert("Erro ao criar refeição: " + error.message);
+    return null;
+  }
+}
+// Fim yuri
